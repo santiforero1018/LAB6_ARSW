a80e9833a1c9e32b3a448842979b3d918d2ab6d4
Santiago Forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-10-01 21:10:50 -0500

    seguir mirando la logica para front end

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,40 @@
+appModule = (function () {
+    let author = '';
+
+    function setAuthorName() {
+        author = $('#theInput').val();
+    }
+
+    function getAuthorName() {
+        return author;
+    }
+
+    function getBluePrints(){
+        apimock.getBlueprintsByAuthor(author, function(data){
+            const bluePrints = data.map(function(blueprint){
+                return {name: blueprint.name, points: blueprint.points.length};
+            });
+
+            $('.table').empty();
+            $('.table').append('<tr><th>BluePrintName</th><th>TotalPoints</th></tr>');
+
+            bluePrints.forEach(function (info) {
+                const newRow = $('<tr>');
+                newRow.append('<td>' + info.name + '</td>');
+                newRow.append('<td>' + info.points + '</td>');
+                $('.table').append(newRow);
+            });
+        });
+    }
+
+    
+
+
+    return {
+        setAuthorName,
+        getAuthorName,
+        getBluePrints
+    }
+
+
+})();
